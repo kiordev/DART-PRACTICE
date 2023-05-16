b0f6ab9f57c1742703e11c534a0f8f0d04ad77be
kiordev <kiordev@gmail.com>
2023-05-16 15:19:23 +0300

    Add Practice

--- bin/Lessons_6_Methods.dart
+++ bin/Lessons_6_Methods.dart
@@ -1,8 +1,23 @@
-// Функции, шаблон обычный void (или тип значения в ретерне) Название (тип данных название)
-// Required - резервирует аргументы, можно менять местами
-void myFunction({required String name, required int date}){
-  print("$name + $date");
-}
+// // Функции, шаблон обычный void (или тип значения в ретерне) Название (тип данных название)
+// // Required - резервирует аргументы, можно менять местами
+// void myFunction({required String name, required int date}){
+//   print("$name + $date");
+// }
+// void main(){
+//   myFunction(date: 17, name: "Sasha");
+// }
+
+// bool canDoIt(int val){
+//   return val>5; // Вариация написания условий для bool
+// }
+// canDoIt(val) => val>5; // Вариация написания функции в одну строку
+
+double calculatePrice(int count, double price){
+   var result = count*price;
+   return result;
+  }
+// Function f1 = (int i)=>i*10;
+
 void main(){
-  myFunction(date: 17, name: "Sasha");
+  print(calculatePrice(10, 5.5));
 }
